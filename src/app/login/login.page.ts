0296f78c3fc8632125b0bdb84527804845a5cb4b
สริกีร์ เอียดตรง <srikee.e@psu.ac.th>
2019-11-12 15:36:45 +0700

    save

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  username: string = "";
+  password: string = "";
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
   }
-
+  login() {
+    let url = "http://localhost/ionic/login.php";
+    this.http.post(url, JSON.stringify({
+      username: this.username,
+      password: this.password
+    }), { responseType: 'text' }).subscribe((res: any) => {
+      //alert(res);
+      let data = JSON.parse(res);
+      if (data.status == true) {
+        this.router.navigateByUrl("/tabs/tab1", { replaceUrl: true });
+      } else {
+        alert("Login ไม่ผ่าน");
+      }
+    })
+  }
 }
