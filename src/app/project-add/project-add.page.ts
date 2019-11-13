9d1184c4b3bd01ff39cdaa0019fad25989f21f3c
สริกีร์ เอียดตรง <srikee.e@psu.ac.th>
2019-11-13 14:59:11 +0700

    save

--- src/app/project-add/project-add.page.ts
+++ src/app/project-add/project-add.page.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-add.page.scss'],
 })
 export class ProjectAddPage implements OnInit {
-  project = {};
+  project: any = {};
   constructor() { }
 
   ngOnInit() {
   }
+  add() {
 
+  }
+  validate() {
+    console.log(this.project.project_name);
+    if (!this.project.project_name) return true;
+    if (!this.project.project_detail) return true;
+    if (!this.project.project_date) return true;
+    if (!this.project.project_amount) return true;
+    return false;
+  }
 }
